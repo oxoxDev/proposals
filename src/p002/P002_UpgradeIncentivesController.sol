eb90305cbf50688477754feda9e0d9ae8a8cb912
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-08-06 00:43:59 +0530

    add proposal to upgrade incentives controller

---
+++ src/p002/P002_UpgradeIncentivesController.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.12;
+
+// ███████╗███████╗██████╗  ██████╗
+// ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗
+//   ███╔╝ █████╗  ██████╔╝██║   ██║
+//  ███╔╝  ██╔══╝  ██╔══██╗██║   ██║
+// ███████╗███████╗██║  ██║╚██████╔╝
+// ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝
+
+// Website: https://zerolend.xyz
+// Discord: https://discord.gg/zerolend
+// Twitter: https://twitter.com/zerolendxyz
+// Telegram: https://t.me/zerolendxyz
+
+// visit https://github.com/zerolend/proposals for information about tests and deployment scripts
+
+import {RewardsController} from "lib/periphery-contracts/contracts/rewards/RewardsController.sol";
+
+
+interface IRewardsController {
+    function upgradeTo(address newImplementation) external;
+}
+contract P002_UpgradeIncentiveController {
+    address private constant EMISSIONS_MANAGER = 0x749dF84Fd6DE7c0A67db3827e5118259ed3aBBa5;
+    address private constant STAKING = 0x2666951A62d82860E8e1385581E2FB7669097647;
+    address private constant REWARDS_CONTROLLER_PROXY = 0x28F6899fF643261Ca9766ddc251b359A2d00b945; 
+
+    IRewardsController rewardsControllerProxy;
+    RewardsController rewardsControllerImpl;
+
+    constructor() {
+        rewardsControllerProxy = IRewardsController(REWARDS_CONTROLLER_PROXY);
+        rewardsControllerImpl = new RewardsController(EMISSIONS_MANAGER, STAKING);
+    }
+
+    function execute() public {
+        rewardsControllerProxy.upgradeTo(address(rewardsControllerImpl));
+    }
+}
