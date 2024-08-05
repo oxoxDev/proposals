21f061549047cf4d7a5c33ad05ba1f5c8b7468d2
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-08-06 00:50:10 +0530

    forge fmt

--- src/p002/P002_UpgradeIncentivesController.sol
+++ src/p002/P002_UpgradeIncentivesController.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.12;
+pragma solidity 0.8.19;
 
 // ███████╗███████╗██████╗  ██████╗
 // ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗
@@ -17,14 +17,14 @@ pragma solidity 0.8.12;
 
 import {RewardsController} from "lib/periphery-contracts/contracts/rewards/RewardsController.sol";
 
-
 interface IRewardsController {
     function upgradeTo(address newImplementation) external;
 }
+
 contract P002_UpgradeIncentiveController {
     address private constant EMISSIONS_MANAGER = 0x749dF84Fd6DE7c0A67db3827e5118259ed3aBBa5;
     address private constant STAKING = 0x2666951A62d82860E8e1385581E2FB7669097647;
-    address private constant REWARDS_CONTROLLER_PROXY = 0x28F6899fF643261Ca9766ddc251b359A2d00b945; 
+    address private constant REWARDS_CONTROLLER_PROXY = 0x28F6899fF643261Ca9766ddc251b359A2d00b945;
 
     IRewardsController rewardsControllerProxy;
     RewardsController rewardsControllerImpl;
