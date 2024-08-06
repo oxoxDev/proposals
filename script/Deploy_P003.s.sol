1cab032d2596a32bd6c01bb6939a66bbe15fe337
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-08-07 01:02:36 +0530

    fix scripts

---
+++ script/Deploy_P003.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.12;
+
+// ███████╗███████╗██████╗  ██████╗
+// ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗
+//   ███╔╝ █████╗  ██████╔╝██║   ██║
+//  ███╔╝  ██╔══╝  ██╔══██╗██║   ██║
+// ███████╗███████╗██║  ██║╚██████╔╝
+// ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝
+
+// Website: https://zerolend.xyz
+// Discord: https://discord.gg/zerolend
+// Twitter: https://twitter.com/zerolendxyz
+// Telegram: https://t.me/zerolendxyz
+
+import { P003_UpdateCliffForAirdrops } from "src/p003/P003_UpdateCliffForAirdrops.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+
+contract Deploy_P003 is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        new P003_UpdateCliffForAirdrops();
+
+        vm.stopBroadcast();
+    }
+}
